15c1cad7d2cdc74859662cabd4fe6d6f52eacf99
Parker Jones <parker.matthewjones@gmail.com>
2021-07-11 19:27:58 -0500

    auto increment

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,40 +1,40 @@
-INSERT INTO departments(id, department_name)
+INSERT INTO departments(department_name)
 VALUES
-(001, "Management"),
-(002, "Engineering"),
-(003, "IT"),
-(004, "Finance"),
-(005, "HR"),
-(006, "Customer Support");
+("Management"),
+("Engineering"),
+("IT"),
+("Finance"),
+("HR"),
+("Customer Support");
 
-INSERT INTO roles (id, title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-(01, "CEO", 450000, 001),
-(02, "President", 300000, 001),
-(03, "VP Finance", 250000, 004),
-(04, "Senior Engineer", 200000, 002),
-(05, "Junior Engineer", 120000, 002),
-(06, "Senior Tech Support", 110000, 003),
-(07, "Junior Tech Support", 85000, 003),
-(08, "Accounting", 90000, 004),
-(09, "HR Manager", 95000, 005),
-(010, "HR Assistant", 65000, 005),
-(011, "Head of Customer Relations", 90000, 006),
-(012, "Customer Support", 60000, 006);
+("CEO", 450000, 001),
+("President", 300000, 001),
+("VP Finance", 250000, 004),
+("Senior Engineer", 200000, 002),
+("Junior Engineer", 120000, 002),
+("Senior Tech Support", 110000, 003),
+("Junior Tech Support", 85000, 003),
+("Accounting", 90000, 004),
+("HR Manager", 95000, 005),
+("HR Assistant", 65000, 005),
+("Head of Customer Relations", 90000, 006),
+("Customer Support", 60000, 006);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-(1, "David", "Gatzby", 01, null),
-(2, "Charlotte", "Webb", 02, 1),
-(3, "Garrett", "Thomson", 03, 1),
-(4, "Theresa", "Jones", 04, 2),
-(5, "Trey", "Williams", 05, 4),
-(6, "Ashley", "Franklin", 05, 4),
-(7, "Trevor", "Smith", 06, 2),
-(8, "Gayle", "Baker", 07, 7),
-(9, "Thomas", "Rodriguez", 08, 3),
-(10, "Patricia", "Stevenson", 09, 2),
-(11, "Danielle", "Johnston", 010, 10),
-(12, "Beverly", "Bellpepper", 011, 2),
-(13, "DJ", "Beepboop", 012, 12);
+("David", "Gatzby", 01, null),
+("Charlotte", "Webb", 02, 1),
+("Garrett", "Thomson", 03, 1),
+("Theresa", "Jones", 04, 2),
+("Trey", "Williams", 05, 4),
+("Ashley", "Franklin", 05, 4),
+("Trevor", "Smith", 06, 2),
+("Gayle", "Baker", 07, 7),
+("Thomas", "Rodriguez", 08, 3),
+("Patricia", "Stevenson", 09, 2),
+("Danielle", "Johnston", 010, 10),
+("Beverly", "Bellpepper", 011, 2),
+("DJ", "Beepboop", 012, 12);
 
--- index.js
+++ index.js
@@ -73,11 +73,6 @@ function startPrompt() {
         inquirer
             .prompt([
                 {
-                    name: "departmentId",
-                    type: "input",
-                    message: "New Department ID: ",
-                },
-                {
                     name: "departmentName",
                     type: "input",
                     message: "New Deparment Name: ",
@@ -86,7 +81,6 @@ function startPrompt() {
 
             .then(function(response) {
                 db.query("INSERT INTO departments SET ?", {
-                    id: response.departmentId,
                     department_name: response.departmentName,
                 },
                     function(err) {
@@ -100,11 +94,6 @@ function startPrompt() {
     function addRole() {
         inquirer
             .prompt([
-                {
-                    name: "roleID",
-                    type: "input",
-                    message: "New Role ID: ",
-                },
                 {
                     name: "roleName",
                     type: "input",
@@ -124,7 +113,6 @@ function startPrompt() {
 
             .then(function(response) {
                 db.query("INSERT INTO roles SET ?", {
-                    id: response.roleID,
                     title: response.roleName,
                     salary: response.roleSalary,
                     department_id: response.roleDepartment,
@@ -140,11 +128,6 @@ function startPrompt() {
     function addEmployee() {
         inquirer
             .prompt([
-                {
-                    name: "employeeID",
-                    type: "input",
-                    message: "New Employee ID: "
-                },
                 {
                     name: "firstName",
                     type: "input",
@@ -169,7 +152,6 @@ function startPrompt() {
 
             .then(function(response) {
                 db.query("INSERT INTO employee SET ?", {
-                    id: response.employeeID,
                     first_name: response.firstName,
                     last_name: response.lastName,
                     role_id: response.employeeRole,
